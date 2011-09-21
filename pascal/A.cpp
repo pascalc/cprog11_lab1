5ae9f40e1181f74905ab62badaf030e69969eb26
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2011-09-21 17:19:55 +0200

    semicolons

--- pascal/A.cpp
+++ pascal/A.cpp
@@ -22,23 +22,23 @@ int main()
 {
     std::cout << "My name is a" << std::endl
     A a("my name is a");
-    std::cout << "1." << std::endl
+    std::cout << "1." << std::endl;
     A b = a;         // vad är skillnaden
-    std::cout << "2." << std::endl
+    std::cout << "2." << std::endl;
     A c(a);          // mellan dessa
-    std::cout << "3." << std::endl
+    std::cout << "3." << std::endl;
     A d;             // tre tekniker?
     d = a;
 
-    std::cout << "noref(a)" << std::endl
+    std::cout << "noref(a)" << std::endl;
     no_ref(a);       // Bildas temporära objekt?
-    std::cout << "noref(b)" << std::endl
+    std::cout << "noref(b)" << std::endl;
     with_ref(a);     // Bildas temporära objekt?
 
-    std::cout << "Array creation" << std::endl
+    std::cout << "Array creation" << std::endl;
     A *aa = new A[5];
-    std::cout << "Array deletion" << std::endl
+    std::cout << "Array deletion" << std::endl;
     delete aa;       // Vad kommer att hända?
-    std::cout << "end main" << std::endl
+    std::cout << "end main" << std::endl;
     return 0;
 }
