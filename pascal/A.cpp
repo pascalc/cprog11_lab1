4932764214495ab020c3d17f2550d036436ea3cf
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2011-09-21 17:31:12 +0200

    Added answer printouts

--- pascal/A.cpp
+++ pascal/A.cpp
@@ -20,25 +20,25 @@ void with_ref(const A & a) {}
 
 int main()
 {
-    std::cout << "My name is a" << std::endl;
+    std::cout << "Some other constructor" << std::endl;
     A a("my name is a");
-    std::cout << "1." << std::endl;
+    std::cout << "1. A b = a <-- copy constructor" << std::endl;
     A b = a;         // vad är skillnaden
-    std::cout << "2." << std::endl;
+    std::cout << "2. A c(a) <--> A c = a <-- copy constructor" << std::endl;
     A c(a);          // mellan dessa
-    std::cout << "3." << std::endl;
+    std::cout << "3. A d; d = a <-- assignment operator" << std::endl;
     A d;             // tre tekniker?
     d = a;
 
-    std::cout << "noref(a)" << std::endl;
+    std::cout << "noref(a) <-- send by value, so temp object created and destroyed" << std::endl;
     no_ref(a);       // Bildas temporära objekt?
-    std::cout << "with_ref(a)" << std::endl;
+    std::cout << "with_ref(a) <-- send by ref so no temp object" << std::endl;
     with_ref(a);     // Bildas temporära objekt?
 
-    std::cout << "Array creation" << std::endl;
+    std::cout << "Array creation <-- default constructor * 5" << std::endl;
     A *aa = new A[5];
-    std::cout << "Array deletion" << std::endl;
-    delete[] aa;       // Vad kommer att hända?
+    std::cout << "delete aa <-- not array delete, so only first A deleted, MEMORY LEAK" << std::endl;
+    delete aa;       // Vad kommer att hända?
     std::cout << "end main" << std::endl;
     return 0;
 }
