e067dfd789b9e73f09794480a73b67c30de3c916
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2011-09-24 18:07:33 +0200

    yeah

--- pascal/vector14/vector.h
+++ pascal/vector14/vector.h
@@ -12,22 +12,11 @@ public:
 	~Vector();
 
 	struct array_proxy {
-		int & ref;
-		array_proxy(int & r) : ref(r) {}
-		void operator = (int value) {
-			ref = value;
-		}
-		operator int() const{
-			return ref;
-		}
-		// Prefix ++
-		int operator ++ () {
-			return ++ref;
-		}
-		// Postfix ++
-		int operator ++ (int i) {
-			return ref++;
-		}
+		array_proxy(int & r);
+		void operator = (int value);
+		operator int() const;
+		int operator ++ ();
+		int operator ++ (int i);
 	};
 
 	// Functions
