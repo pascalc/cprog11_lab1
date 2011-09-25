4e348e29ffa3e0ffebff25ce18c865f6c2a3d371
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2011-09-25 23:33:22 +0200

    yo

--- pascal/vector15/vector.cpp
+++ pascal/vector15/vector.cpp
@@ -4,10 +4,8 @@
 using namespace std;
 
 int main(){
-	Vector<double> v = Vector<double>(1);
-	v.clear();
-	v.insert(0,1);
-	for(unsigned int i = 0; i < v.size(); i++)
-		cout << v[i] << endl;
+	Vector<int> ivec;
+        for(int i = 0; i < 10; i++)
+            ivec.insert(9-i, i);
 	return 0;
 }
